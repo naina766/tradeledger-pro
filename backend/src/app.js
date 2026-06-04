b4bfc4852f393b24d461cd4df21bd4056146bf73
Naina Varshney <nainavarshney842@gmail.com>
2026-06-04 13:24:17 +0530

    fix prisma dependency

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,9 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
 const express = require('express');
 const cors = require('cors');
+
 const helmet = require('helmet');
 const morgan = require('morgan');
 const logger = require('./utils/logger');
